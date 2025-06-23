d79086f6a1ba6b9272f69609ff74e0f76c08cb30
Orhan Güzel <orhan.guezel@dci-student.org>
2025-06-23 09:30:45 +0200

    deploying

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,18 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// Bu kısımlar dosyanın doğru şekilde çalışması için gerekli
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Vite yapılandırması
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, 
+    port: 5173,
+    open: true,
   },
   resolve: {
     alias: {
@@ -18,4 +20,3 @@ export default defineConfig({
     },
   },
 });
-
